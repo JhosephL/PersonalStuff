eb3ccaf808ddf547e45cc5dafba9f723f81f0666
Jhoseph Lizarazo <jhosephslizarazom@outlook.com>
2022-08-24 23:54:10 -0500

    Adding a new C task.

---
+++ C/C_SoloLearn (In progress)/main.c
@@ -0,0 +1,71 @@
+
+// Header file
+# include <stdio.h>
+
+
+int main ( ) {
+
+
+
+
+    printf ("\n\n\n\n\nc c c c c c c c c c c c c c c c c c c c c c c c c\tTHIS IS SOLOLEARN'S C PROGRAMMING LANGUAGE COURSE\tc c c c c c c c c c c c c c c c c c c c c c c c c\n\n\n\n\n") ;
+
+
+
+
+    printf ("  - - - - -  >\tESCAPE CHARACTERS") ;
+    printf ("\n\n\n") ;
+
+    printf (
+    
+        "\t\t--------------- ---------------------------\t\t\n"
+        "\t\t|      \\a      |    Alarm or beep.        |\n"
+        "\t\t|      \\b      |    Backspace.            |\n"
+        "\t\t|      \\f      |    Form feed.            |\n"
+        "\t\t|      \\n      |    New line.             |\n"
+        "\t\t|      \\r      |    Carriage return.      |\n"
+        "\t\t|      \\t      |    Horizontal tab.       |\n"
+        "\t\t|      \\v      |    Vertical tab.         |\n"
+        "\t\t|      \\\\      |    Backslash.            |\n"
+        "\t\t|      \\'      |    Single quote.         |\n"
+        "\t\t|      \\\"      |    Double quote.         |\n"
+        "\t\t|      \\?      |    Question mark.        |\n"
+        "\t\t|      \\ooo    |    Octal number.         |\n"
+        "\t\t|      \\xhh    |    Hexadecimal number.   |\n"
+        "\t\t|      \\0      |    Null.                 |\n"
+        "\t\t--------------- ---------------------------\t\t"
+            
+    ) ;
+
+
+    printf ("\n\n\n\n") ;
+
+
+    printf ("  - - - - -  >\tDATA TYPES") ;
+    printf ("\n\n\n") ;
+
+    printf ("\t\t// CODE") ;
+
+
+    printf ("\n\n\n\n") ;
+
+
+    printf ("  - - - - -  >\tTRYING  . . .") ;
+    printf ("\n\n\n") ;
+
+    printf ("\t\t// CODE") ;
+
+
+
+
+    printf ("\n\n\n\n\nc c c c c c c c c c c c c c c c c c c c c c c c c\tTHIS IS SOLOLEARN'S C PROGRAMMING LANGUAGE COURSE\tc c c c c c c c c c c c c c c c c c c c c c c c c\n\n\n\n\n\n") ;
+
+
+
+
+return 0;
+}
+
+
+
+// To rest -->    
