f015cb600cc717869f4873e8abc5ebac93507b28
Jhoseph Lizarazo <jhosephslizarazom@outlook.com>
2023-06-30 12:40:38 -0500

    Upload Kotlin knowledge.

---
+++ Kotlin/Kotlin_MainFormat/main.kt
@@ -0,0 +1,27 @@
+
+// Package
+
+
+// Imports
+
+
+
+fun main ( ) {
+
+
+    println (function())
+
+
+}
+
+
+fun function ( ) : String {
+
+    return "\n\n\t// CODE\n\n"
+
+}
+
+
+
+
+// To rest --->-->->    
