4d94c1d52aac574bc00e3be3e61aabf47a6339ae
JhosephL <jhosephslizarazom@outlook.com>
2023-11-23 03:27:58 -0500

    Adding two new files.

---
+++ Shell/Files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create empty files.
+> "file name"
+touch "files name"
+
+# ECHO
+## Overwrite.
+echo "content" > "file name"
+## Append.
+echo "content" >> "file name"
+
+# CAT
+cat > "file name"
+cat >> "file name"
--- Shell/initial.sh
+++ Shell/initial.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+echo
+echo
+
 sudo apt-get update
 sudo apt update
+
+echo
 
 sudo apt-get upgrade -y
 sudo apt upgrade -y
 
+echo
+
 sudo apt-get install --reinstall
 sudo apt install --reinstall
+
+echo
 
 sudo apt-get autoremove -y
 sudo apt autoremove -y
 
+echo
+
 sudo apt-get clean
 sudo apt-get clean
+
+echo
+
+sudo screenfetch
+
+echo
+echo
