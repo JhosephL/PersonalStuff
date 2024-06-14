dbeebd3426f7c39d13a182bf5c849f13f9c5d9b2
Jhoseph Lizarazo <jhosephslizarazom@outlook.com>
2024-06-13 22:10:37 -0500

    Add TS knowledge.

---
+++ TS/TS_MainFormat/main.ts
@@ -0,0 +1,18 @@
+
+//// CALLING.
+function_main() ;
+
+
+//// MAIN FUNCTION.
+function function_main ( ) : void {
+
+    function_print() ;
+
+}
+
+
+//// OTHER FUNCTIONS.
+function function_print ( ) : void {
+    console.log ( "\n\n\tCODE\n" ) ;
+}
+
